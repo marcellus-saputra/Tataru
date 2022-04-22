9454276dcdfaf148a33a7e6e72c6bf5ee9fab361
Marcellus Saputra <marcellussaputra@gmail.com>
2022-04-22 21:17:12 +0200

    Added bulk price checking

--- Tataru_main.py
+++ Tataru_main.py
@@ -6,6 +6,7 @@ import random
 import requests
 import asyncio
 from lib import bj_mw
+from lib import poe_trade
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,6 +15,9 @@ GUILD = os.getenv('DISCORD_GUILD')
 doujin_max = 1000000
 
 bot = commands.Bot(command_prefix="%")
+
+print("Initializing PoE Trade integration...")
+trade = poe_trade.Poe_trade()
 
 bj_model = bj_mw.Blackjack_mw()
 
@@ -32,7 +36,7 @@ async def bj_chance(ctx, player_hand, player_has_ace, dealer_hand):
         dealer_hand = int(dealer_hand)
         dealer_has_ace = dealer_hand == 11
     except:
-        response = f'Tatary says:\n> Invalid input. Please input your game state as follows: ' \
+        response = f'Tataru says:\n> Invalid input. Please input your game state as follows: ' \
                    f'"<your hand> <T or t if you have an ace, anything else otherwise> <dealer hand>.'
         await ctx.send(response)
         return
@@ -44,6 +48,15 @@ async def bj_chance(ctx, player_hand, player_has_ace, dealer_hand):
 @bot.command(name="roll")
 async def roll(ctx, *args):
     response = f'Tataru says:\n> {random.choice(args)}'
+    await ctx.send(response)
+
+@bot.command(name="bulk_check")
+async def bulk_check(ctx, item):
+    listings = trade.price_check_bulk(item)
+    response = 'Tataru says:\n'
+    for listing in listings:
+        response += f'Price: {listing[0]} \t exalt \t Stock: {listing[1]}\n'
+    response = response[:-1]
     await ctx.send(response)
 
 @bot.command(name="doujin_no")
@@ -103,6 +116,5 @@ async def doujin(ctx):
         id_exists = True
     response = f'Tataru recommends (after {tries} tries):\n{url}'
     await ctx.send(response)
-
 
 bot.run(TOKEN)
